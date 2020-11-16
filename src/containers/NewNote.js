82ebd1b1aafcb4a38aa91c1901ae697f6623dfa1
izzhariz <izzharizdev@gmail.com>
2020-11-16 14:37:56 +0800

    fixing deploement errors

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -36,7 +36,7 @@ export default function NewNote() {
         try {
             const attachment = file.current ? await s3Upload(file.current) : null;
 
-            await createNote({ content });
+            await createNote({ content, attachment });
             history.push("/");
         } catch (e) {
             onError(e);
